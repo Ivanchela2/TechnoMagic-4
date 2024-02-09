a088083e33b728df41bd142c9c37725b45cf5e5c
kraneeeee <129248399+kraneeeee@users.noreply.github.com>
2024-02-09 22:23:21 +1000

    init

--- kubejs/startup_scripts/stage 7/item_registry.js
+++ kubejs/startup_scripts/stage 7/item_registry.js
@@ -1,32 +1,54 @@
 StartupEvents.registry('item', e => {
-    e.create('ad_astra:reinforced_rocket_fin').displayName('§6Reinforced Rocket Fin')
-    e.create('ad_astra:reinforced_rocket_nose_cone').displayName('§6Reinforced Rocket Nose Cone')
-    e.create('ad_astra:advanced_rocket_fin').displayName('§5Advanced Rocket Fin')
-    e.create('ad_astra:advanced_rocket_nose_cone').displayName('§5Advanced Rocket Nose Cone')
-    e.create('ad_astra:ultra_rocket_fin').displayName('§cUltra Rocket Fin')
-    e.create('ad_astra:ultra_rocket_nose_cone').displayName('§cUltra Rocket Nose Cone')
-    e.create('ad_astra:incomplete_reinforced_rocket_fin','create:sequenced_assembly').displayName('Incomplete Reinforced Rocket Fin')
-    e.create('ad_astra:incomplete_advanced_rocket_fin','create:sequenced_assembly').displayName('Incomplete Advanced Rocket Fin')
-    e.create('ad_astra:incomplete_ultra_rocket_fin','create:sequenced_assembly').displayName('Incomplete Ultra Rocket Fin')
-    e.create('botania:infinity_petal').displayName('§dInfinity Petal')
-    e.create('botania:gaia_catalyst').displayName('§7Gaia Catalyst')
-    e.create('botania:maximum_catalyst').displayName('§bMaximum Catalyst')
-    e.create('chemlib:terrasteel_element').displayName('§aTerrasteel').group('chemlib.elements').tooltip('§bTr (500)').tooltip('§7Transition Metals')
-    e.create('chemlib:gaia_element').displayName('§dGaia').group('chemlib.elements').tooltip('§bGi (320)').tooltip('§7Transition Metals')
-    e.create('chemlib:mana_element').displayName('§9Mana').group('chemlib.elements').tooltip('§bMa (3000)').tooltip('§7Transition Metals')
-    e.create('chemlib:botany_compound').displayName('§5Botany Compound').group('chemlib.compounds').tooltip('§3Ma₂Tr₂Gi')
-    e.create('botania:gaia_punishment').displayName('§3Gaia Punishment')
-    e.create('botania:terrasteel_potency').displayName('§aTerrasteel Potency')
-    e.create('botania:elves_endowment').displayName('§dElves Endowment')
-    e.create('botania:incomplete_terra_pick','create:sequenced_assembly').displayName('Incomplete Terra Shatterer')
-    e.create('botania:incomplete_terra_axe','create:sequenced_assembly').displayName('Incomplete Terra Truncator')
-    e.create('botania:incomplete_terra_sword','create:sequenced_assembly').displayName('Incomplete Terra Sword')
-    e.create('botania:rune_base').displayName('§dRune Base');
-    e.create('botania:rune_suspension').displayName('§dRune Suspension');
-    e.create('botania:botany_catalyst').displayName('§dBotany Catalyst');
-    e.create('create:steam_power_generator').displayName('Steam Power Generator');
-    e.create('botania:specific_rune_base').displayName('§dSpecific Rune Base');
-    e.create('botania:basic_runic_stone').displayName('§9Basic Runic Stone');
-    e.create('botania:purple_piece_of_specific_rune').displayName('§dPurple Piece of Specific Rune');
-    e.create('botania:blue_piece_of_specific_rune').displayName('§bBlue Piece of Specific Rune');
-})
+    const items = {
+        'buffalo_hide_fiber': 'Волокно шкуры бизона',
+        'buffalo_hide_rope': 'Веревка из шкуры бизона',
+        'baykok_bone': 'Кость Байкока',
+        'ad_astra:reinforced_rocket_fin':'§6Reinforced Rocket Fin',
+        'ad_astra:reinforced_rocket_nose_cone':'§6Reinforced Rocket Nose Cone',
+        'ad_astra:advanced_rocket_fin':'§5Advanced Rocket Fin',
+        'ad_astra:advanced_rocket_nose_cone':'§5Advanced Rocket Nose Cone',
+        'ad_astra:ultra_rocket_fin':'§cUltra Rocket Fin',
+        'ad_astra:ultra_rocket_nose_cone':'§cUltra Rocket Nose Cone',
+        'ad_astra:incomplete_reinforced_rocket_fin':'Incomplete Reinforced Rocket Fin',
+        'ad_astra:incomplete_advanced_rocket_fin':'Incomplete Advanced Rocket Fin',
+        'ad_astra:incomplete_ultra_rocket_fin': 'Incomplete Ultra Rocket Fin',
+        'botania:infinity_petal': '§dInfinity Petal',
+        'botania:gaia_catalyst': '§7Gaia Catalyst',
+        'botania:maximum_catalyst': '§bMaximum Catalyst',
+        'botania:gaia_punishment': '§3Gaia Punishment',
+        'botania:terrasteel_potency': '§aTerrasteel Potency',
+        'botania:elves_endowment':'§dElves Endowment',
+        'botania:incomplete_terra_pick':'Incomplete Terra Shatterer',
+        'botania:incomplete_terra_axe':'Incomplete Terra Truncator',
+        'botania:incomplete_terra_sword':'Incomplete Terra Sword',
+        'botania:rune_base':'§dRune Base',
+        'botania:rune_suspension':'§dRune Suspension',
+        'botania:botany_catalyst':'§dBotany Catalyst',
+        'create:steam_power_generator':'Steam Power Generator',
+        'botania:specific_rune_base':'§dSpecific Rune Base',
+        'botania:basic_runic_stone':'§9Basic Runic Stone',
+        'botania:purple_piece_of_specific_rune':'§dPurple Piece of Specific Rune',
+        'botania:blue_piece_of_specific_rune':'§bBlue Piece of Specific Rune'
+    };
+
+    for (const [id,dName] of Object.entries(items)) {
+
+        let name;
+        if (!id.includes(':')) {
+            name = `gribtweaks:${id}`
+        } else name = id;
+        
+        e.create(name).displayName(dName);
+    };
+
+    const create = (id, dName) => e.create(id).displayName(dName);
+
+    e.create('gribtweaks:baykok_bone_pickaxe', 'pickaxe')
+        .displayName('Кирка из кости Байкока')
+        .tier('diamond');
+
+    create('chemlib:terrasteel_element','§aTerrasteel').group('chemlib.elements').tooltip('§bTr (500)').tooltip('§7Transition Metals')
+    create('chemlib:gaia_element','§dGaia').group('chemlib.elements').tooltip('§bGi (320)').tooltip('§7Transition Metals')
+    create('chemlib:mana_element','§9Mana').group('chemlib.elements').tooltip('§bMa (3000)').tooltip('§7Transition Metals')
+    create('chemlib:botany_compound','§5Botany Compound').group('chemlib.compounds').tooltip('§3Ma₂Tr₂Gi')
+});
