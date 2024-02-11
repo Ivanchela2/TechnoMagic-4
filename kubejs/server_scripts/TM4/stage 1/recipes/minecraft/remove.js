d9f96b218df9072130eee9c56dcdb172d99c05fd
qq1pvl <lol22335lol@gmail.com>
2024-02-12 00:52:04 +0700

    dc fixes

--- kubejs/server_scripts/TM4/stage 1/recipes/minecraft/remove.js
+++ kubejs/server_scripts/TM4/stage 1/recipes/minecraft/remove.js
@@ -1,7 +1,8 @@
 ServerEvents.recipes(event => {
     //����
     event.remove([
-        { output: 'minecraft:crafting_table' },
-        { output: 'crafting_on_a_stick:crafting_table' }
+        { output: 'crafting_on_a_stick:crafting_table' },
+        { output: 'quark:blackstone_furnace' },
+        { output: 'quark:deepslate_furnace' },
     ])
 })
---
+++ kubejs/server_scripts/TM4/stage 3/chipped/shaped.js
@@ -0,0 +1,25 @@
+ServerEvents.recipes((event) => {
+  const prefix = 'gribtweaks:/shaped/';
+  const recipes = [
+    { // Воронка
+      output: 'chipped:carpenters_table', 
+      pattern: [
+        '  A', 
+        'BCB', 
+        'TDB'
+        ],
+      key: {
+        A: 'minecraft:iron_ingot', 
+        B: '#minecraft:logs',
+        C: 'minecraft:crafting_table',
+        D: '#forge:planks',
+        T: '#forge:tools/axes'
+      },
+      id: `${prefix}carpenters_table`
+    },
+  ];
+
+    recipes.forEach((recipe) => {
+        event.shaped(recipe.output, recipe.pattern, recipe.key).id(recipe.id);
+    });
+});
